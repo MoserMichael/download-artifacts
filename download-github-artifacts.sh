9a944892421b4110c1ea2e37c7c67779e19e7c2a
mmoser-blaze <mmoser@perforce.com>
2022-11-28 01:22:02 +0200

    check if required programs are in path, check if output dir exists

--- download-github-artifacts.sh
+++ download-github-artifacts.sh
@@ -30,6 +30,23 @@ EOF
 exit 1
 }
 
+# True iff all arguments are executable in $PATH , from: https://stackoverflow.com/questions/6569478/detect-if-executable-file-is-on-users-path
+function assert_bin_in_path {
+  if [[ -n $ZSH_VERSION ]]; then
+    builtin whence -p "$1" &> /dev/null
+  else  # bash:
+    builtin type -P "$1" &> /dev/null
+  fi
+  if [[ $? != 0 ]]; then
+    echo "Error: $1 is not in the current path"
+    exit 1
+  fi    
+  if [[ $# -gt 1 ]]; then
+    shift  # We've just checked the first one
+    assert_bin_in_path "$@"
+  fi
+}
+
 USER=""
 REPO=""
 DIR="."
@@ -59,6 +76,8 @@ while getopts "hvu:r:o:" opt; do
    esac
 done	
 
+assert_bin_in_path "jq" "curl"
+
 if [[ $USER == "" ]]; then
     Help "user not given"
 fi
@@ -66,6 +85,14 @@ fi
 if [[ $REPO == "" ]]; then
     Help "repository not given"
 fi
+
+if [[ $DIR != "" ]]; then 
+    if [[ ! -d "$DIR" ]]; then
+        echo "Error: output directory $DIR does not exist"
+        exit 1
+    fi
+fi     
+
 
 LATEST_TAG=$(curl -L -s -S  -H "Accept: application/json" "https://github.com/${USER}/${REPO}/releases/latest" | jq --raw-output .tag_name)
 
